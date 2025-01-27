9e7e63900b876b458a843cc282ab2ebf4a9d8822
VishnuBalaji2004 <vishnubalaji570@gmail.com>
2025-01-27 23:56:09 +0530

    Code Generator Created

--- Day_6_Assessment/chain.py
+++ Day_6_Assessment/chain.py
@@ -20,29 +20,3 @@ def Generate_code(language,problem_statement):
         "problem_statement": problem_statement
     })
     return response.content
-
-# from model import create_chat_gorq
-# from prompt import Code_Generator_prompt
-
-# def Generate_code(language, problem_statement):
-#     """
-#     Function to initialize chat gorq
-#     Args:
-#         language (str) - programming language for which code is to be generated
-#         problem_statement (str) - the problem statement for the code generation
-#     Returns:
-#         response.content (str) - generated code as a response
-#     """
-#     print(f"Language: {language}")
-#     print(f"Problem Statement: {problem_statement}")
-
-#     prompt_template = Code_Generator_prompt()
-#     llm = create_chat_gorq()
-    
-#     chain = prompt_template | llm
-    
-#     response = chain.invoke({
-#         "programming_language": language,
-#         "problem_statement": problem_statement
-#     })
-#     return response.content
