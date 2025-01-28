3311695fc4281a68439c3fc781a22f274ff9c367
VishnuBalaji2004 <vishnubalaji570@gmail.com>
2025-01-28 15:54:17 +0530

    Day 6 Completed

--- Day_6_Assessment/prompt.py
+++ Day_6_Assessment/prompt.py
@@ -19,14 +19,14 @@ def Code_Generator_prompt():
 #         ChatPromptTemplate -> Configured ChatPromptTemplate instance
 #     """
 #     system_msg = '''
-#                 You are a dedicated code generator assistant, specialized in crafting code solutions in various programming languages. Your task is strictly to generate code based on the given problem statement and specified programming language. Follow these guidelines:
-#                 1. Only respond to queries explicitly requesting a code solution in a specific programming language.
-#                 2. The output must strictly be the code itself, formatted correctly with proper indentations, without additional explanations, descriptions, or headers.
-#                 3. If the query is unrelated to code generation (e.g., generating poems, recipes, suggestions, general knowledge questions, or any other non-coding tasks), respond with:
-#                 "I am a code generator assistant, expert in generating code solutions in various programming languages. Please ask me a code-related query."
-#                 4. Do not perform any tasks beyond code generation. Always fall back to the above message for non-code-related queries.
-#                 Note: The assistant must ensure the generated code aligns with the requested programming language and problem statement.
-#                 '''
+                # You are a dedicated code generator assistant, specialized in crafting code solutions in various programming languages. Your task is strictly to generate code based on the given problem statement and specified programming language. Follow these guidelines:
+                # 1. Only respond to queries explicitly requesting a code solution in a specific programming language.
+                # 2. The output must strictly be the code itself, formatted correctly with proper indentations, without additional explanations, descriptions, or headers.
+                # 3. If the query is unrelated to code generation (e.g., generating poems, recipes, suggestions, general knowledge questions, or any other non-coding tasks), respond with:
+                # "I am a code generator assistant, expert in generating code solutions in various programming languages. Please ask me a code-related query."
+                # 4. Do not perform any tasks beyond code generation. Always fall back to the above message for non-code-related queries.
+                # Note: The assistant must ensure the generated code aligns with the requested programming language and problem statement.
+                # '''
 #     user_msg = "Write a code in {programming_language} to solve the problem: {problem_statement}"
 #     prompt_template = ChatPromptTemplate([
 #         ("system", system_msg),
